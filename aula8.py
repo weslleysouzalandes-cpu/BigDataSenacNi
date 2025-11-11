1c39648a19b80515fc2a878f98e92b13d6b5ef7b
WESLLEY DE SOUZA LANDES PINHEIRO <weslley.pinheiro@aluno.senac.rio>
2025-11-10 21:51:06 -0300

    Aula 9, verificar while, true e false

--- aula8.py
+++ aula8.py
@@ -40,40 +40,142 @@
 
 ### Calculo do IMC ###
 
-def calc_imc(peso,altura):
+# def calc_imc(peso,altura):
 
-    imc= float(peso/altura*altura)
+#     imc= peso/(altura*altura)
     
-    return imc
+#     return imc
 
 
-def obter_classificacao(imc):
-    if imc< 18.5:
-        resultado = print(f'Seu imc é {imc}, você está abaixo do peso')
-    elif 18.5<=imc<=24.9:
-        resultado = print(f"Peso normal")
-    elif 25.0<=imc<=29.9:
-        resultado=(f"Você está com Sobrepeso")
+# def obter_classificacao(imc):
+#     if imc< 18.5:
+#         resultado = print(f'Seu imc é {imc}, você está abaixo do peso')
+#     elif 18.5<=imc<=24.9:
+#         resultado = print(f"Peso normal")
+#     elif 25.0<=imc<=29.9:
+#         resultado=(f"Você está com Sobrepeso")
+#     else:
+#         resultado = print("Obesidade")
+    
+#     return resultado
+
+
+
+# total_de_pessoas=int(input('Quantas pessoas precisam calcular seu IMC??  '))
+
+# for i in range(1,total_de_pessoas+1):
+#     print(f"\n Você está calculando os dados da pessoa {i} ")
+    
+#     peso=float(input('Digite o peso (kg) desta pessoa:  '))
+#     altura=float(input('Digite a altura(m) desta pessoa em centímetros: '))
+
+#     imc_impresso= calc_imc(peso,altura)
+#     classificacao= obter_classificacao(imc_impresso)
+
+#     print(f"O seu imc é {imc_impresso} e sua classificacao é {classificacao} ")
+
+
+### VAMOS CONVERTER A TEMPERATURA ###
+
+# def celsius_para_f(c):
+#     calculo1= ((c*9)/9)+32
+
+#     return calculo1
+
+
+# def fahrenheit_para_c(f):
+#     calculo2=((f-32)*5)/9
+
+#     return calculo2
+
+# conversao= str(input(" Para qual unidade você deseja fazer? celsius ou fahrenheit "))
+# temperatura = float(input(" Digite a temperatura:  "))
+
+# resultado_celsius= fahrenheit_para_c(temperatura)
+# resultado_f= celsius_para_f(temperatura)
+
+# if conversao== "celsius":
+#     print(f"A sua temperatura em celsius é: {resultado_celsius}")
+# elif conversao == "fahrenheit":
+#     print(f"A sua temperatura em fahrenheit é {resultado_f} ")
+
+# else:
+#     print("Digite a temperatura corretamente")
+
+
+## DESAFIO DA AULA 8, EXERCICIO 3 ##
+# def calculo_temp(x,c):
+#     if x =="fahrenheit":
+#         calculo1= ((c*9)/5)+32
+#         resultado=print(f"A sua temperatura é {calculo1}")
+#     elif x =="celsius":
+#         calculo2=((c-32)*5)/9
+#         resultado=print(f"A sua temperatura é {calculo2}")
+#     else:
+#         resultado=print("Digite uma temperatura corretamente")
+
+#     return resultado
+
+# conversao= str(input(" Para qual unidade você deseja fazer? celsius ou fahrenheit "))
+# temperatura = float(input(" Digite a temperatura:  "))
+
+# print(f"A temperatura é: {calculo_temp(conversao,temperatura)}")
+
+#__________________________________________________________________
+
+## EXERCICIO 1 ANO BISSEXTO  ##
+
+def e_bissexto(ano):
+    
+    ano1= ano%4
+    ano100=ano%100
+    ano400=ano%400
+    if ano1 == 0 and ano100 !=0:
+        resultado=print(f"Seu ano {ano} é bissexto")
+    elif ano1 == 0 and ano100 == 0 and ano400 == 0:
+        resultado = print(f"Seu ano {ano} é bissexto")
     else:
-        resultado = print("Obesidade")
-    
+        resultado=print(f"Seu ano {ano} não é bissexto")
+
+
     return resultado
 
+pergunta=int(input("Digite o ano que você deseja saber"))
+
+final=e_bissexto(pergunta)
 
 
-
-total_de_pessoas=int(input('Quantas pessoas precisam calcular seu IMC??  '))
-
-for i in range(total_de_pessoas):
-    print(f"Você está calculando {i+1} de {total_de_pessoas}")
-    
-
-    
-    peso=int(input('Digite o peso desta pessoa:  '))
-    altura=int(input('Digite a altura desta pessoa em centímetros: '))
+#_____________________________________________________________________
 
 
-imc_impresso= calc_imc(peso,altura)
-classificacao= obter_classificacao(imc_impresso)
+## EXERCICIO 2 CONTAGEM DE CARACTERES  ##
 
-print(f"O seu imc é {imc_impresso} e sua classificacao é {classificacao} ")
+# def contar_caracteres(x,y):
+#     minha_string = x
+#     minuscula= minha_string.lower()
+
+#     resultado = print(f"A lestra {y} se repete {minuscula.count(y)} vezes")
+
+#     return resultado
+
+
+# print(" Vamos contar as letrinhas?? ")
+# texto=str(input("\n Digite o eu texto:  "))
+# caractere_proc= str(input("Digite a letra procurada:  "))
+
+# final=contar_caracteres(texto,caractere_proc)
+
+#__________________________________________________________________
+# import random
+# ## Simulador de Dado 
+
+# def rolar_dado(lados):
+#     aleatorio= random.randint(1,lados)
+
+#     return aleatorio
+
+
+# ataque=rolar_dado(20)
+# dano=rolar_dado(8)
+
+# print(f"Seu ataque é {ataque} e dano {dano}")
